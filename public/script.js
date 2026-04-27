e156505a167146be7b5a8e2e1d01061c2ad6d6b6
huzaifa <idkmyname9009@gmail.com>
2026-04-27 20:59:18 +0530

    initial commit

---
+++ public/script.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/12.12.1/firebase-app.js";
+  // TODO: Add SDKs for Firebase products that you want to use
+  // https://firebase.google.com/docs/web/setup#available-libraries
+
+  // Your web app's Firebase configuration
+  const firebaseConfig = {
+    apiKey: "AIzaSyBHdpGGco8bBq_G0kUUFReU9LkZbMWYxVU",
+    authDomain: "test-bc4c0.firebaseapp.com",
+    projectId: "test-bc4c0",
+    storageBucket: "test-bc4c0.firebasestorage.app",
+    messagingSenderId: "166653353895",
+    appId: "1:166653353895:web:3ea8d7c3640ce282876082"
+  };
+
+  // Initialize Firebase
+  const app = initializeApp(firebaseConfig);
